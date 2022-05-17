0135dd9398adac61388f9ba6ccbb929adf60d47e
Antony <ac.chan@live.com>
2022-05-17 06:57:05 +0000

    continue typescript practice

--- typescript-test/main.js
+++ typescript-test/main.js
@@ -1,14 +1,49 @@
+/*
 function log(message) {
-    console.log(message);
+  console.log(message);
 }
+
 var message = 'Hello World';
+
 log(message);
+
 var number = 1;
-var count = 2;
+let count = 2;
+
+
 function doSomething() {
-    for (var i = 0; i < 5; i++) {
-        console.log(i);
-    }
-    console.log('Finally: ' + i);
+  for (let i = 0; i < 5; i++) {
+    console.log(i);
+  }
+  console.log('Finally: ' + i);
 }
-log(doSomething);
+
+doSomething();
+
+
+let count = 5;
+
+count = 'a';
+
+let a: number;
+a = 1;
+a = true;
+a = 'a';
+*/
+var a;
+var b;
+var c;
+var d;
+var e = [1, 2, 3];
+var f = [1, true, 'a', false];
+var ColorRed = 0;
+var ColorGreen = 1;
+var COlorBlue = 2;
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 1] = "Green";
+    Color[Color["Blue"] = 2] = "Blue";
+    Color[Color["Purple"] = 3] = "Purple";
+})(Color || (Color = {}));
+var backgroundColor = Color.Red;
--- typescript-test/main.ts
+++ typescript-test/main.ts
@@ -1,3 +1,4 @@
+/*
 function log(message) {
   console.log(message);
 }
@@ -9,11 +10,42 @@ log(message);
 var number = 1;
 let count = 2;
 
+
 function doSomething() {
-  for (var i = 0; i < 5; i++) {
+  for (let i = 0; i < 5; i++) {
     console.log(i);
   }
   console.log('Finally: ' + i);
 }
 
-log(doSomething);
+doSomething();
+
+
+let count = 5;
+
+count = 'a';
+
+let a: number;
+a = 1;
+a = true;
+a = 'a';
+*/
+
+let a: number;
+let b: boolean;
+let c: string;
+let d: any;
+let e: number[] = [1, 2, 3];
+let f: any[] = [1, true, 'a', false];
+
+const ColorRed = 0;
+const ColorGreen = 1;
+const COlorBlue = 2;
+
+enum Color {
+  Red = 0,
+  Green = 1,
+  Blue = 2,
+  Purple = 3,
+}
+let backgroundColor = Color.Red;
