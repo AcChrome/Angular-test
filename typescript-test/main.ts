969097d86f5ba3fcc0a1a344ff188e3deb09d131
Antony <ac.chan@live.com>
2022-06-03 17:23:46 +0000

    property as a method

--- typescript-test/main.ts
+++ typescript-test/main.ts
@@ -149,7 +149,7 @@ class Point {
 
 let point = new Point(1, 2);
 point.draw();
-*/
+
 
 //Access modifier paramenters
 
@@ -162,4 +162,29 @@ class Point {
 }
 
 let point = new Point(1, 2);
+point.draw();
+*/
+
+// Properties
+
+class Point {
+  constructor(private _x?: number, private _y?: number) {}
+
+  draw() {
+    console.log('X: ' + this._x + ', Y: ' + this._y);
+  }
+
+  get x() {
+    return this._x;
+  }
+
+  set x(value) {
+    if (value < 0) throw new Error('value cannot be less than 0.');
+    this._x = value;
+  }
+}
+
+let point = new Point(1, 2);
+let x = point.x;
+point.x = 10;
 point.draw();
