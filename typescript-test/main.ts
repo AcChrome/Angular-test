b685af5148dfb336f854e8f425b8afb4d889ad6c
Antony <ac.chan@live.com>
2022-06-03 17:13:02 +0000

    access modifer in typescript

--- typescript-test/main.ts
+++ typescript-test/main.ts
@@ -133,5 +133,19 @@ point.draw();
 
 // Access modifier
 
+class Point {
+  private x: number;
+  private y: number;
+
+  constructor(x?: number, y?: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  draw() {
+    console.log('X: ' + this.x + ', Y: ' + this.y);
+  }
+}
+
 let point = new Point(1, 2);
 point.draw();
